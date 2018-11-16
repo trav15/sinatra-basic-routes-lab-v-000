d16c4c338d7ab7c728a0661f00079a4ab46b2607
Travis T <travistokuyama@gmail.com>
2018-11-16 02:51:17 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    'My name is DEEZ'
+  end
+
+  get '/hometown' do
+    "My hometown is DEEZWORLD"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is DEEZ NUTS! off Dr Dre - The Chronic (1992)"
+  end
+
 end
