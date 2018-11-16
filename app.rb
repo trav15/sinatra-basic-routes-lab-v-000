73c59c54bfa9480ad9593e0919fb937003eee851
Travis T <travistokuyama@gmail.com>
2018-11-16 02:51:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,15 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
-get '/name' do
-  'My name is DEEZ'
-end
+  
+  get '/name' do
+    'My name is DEEZ'
+  end
 
-get '/hometown' do
-  "My hometown is DEEZWORLD"
-end
+  get '/hometown' do
+    "My hometown is DEEZWORLD"
+  end
 
-get '/favorite-song' do
-  "My favorite song is DEEZ NUTS! off Dr Dre - The Chronic (1992)"
+  get '/favorite-song' do
+    "My favorite song is DEEZ NUTS! off Dr Dre - The Chronic (1992)"
+  end
+
 end
